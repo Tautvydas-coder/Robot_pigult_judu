4ecfbcc1593ee16653cf76d6bd4ca81a417cd1f3
Tautvydas-coder <b7&*%13kHqvP>
2022-08-03 12:11:42 +0300

    edited

--- Resources/variables.py
+++ Resources/variables.py
@@ -1,12 +1,24 @@
 # pigu.robot
+URL = "https://pigu.lt/lt/"
 ENTER = "\\13"
 PRODUCT_NAME = "Alkotesteris"
 NUMBER = "[9]"
 PRODUCT = "xpath:/html/body/div[1]/div[8]/div[1]/section/div/div/div[2]/section/div[3]/div/div"
 TYPE = "/div/div/div[5]/div[2]"
 CART_BUTTON = "/div/div/a[2]"
+ACCEPT_COOKIES = "xpath:/html/body/div[1]/div[1]/div[1]/div/div/div[2]/button"
+SEARCH_BAR = "xpath:/html/body/div[1]/div[1]/div/div[1]/div/div/div[3]/div[2]/form/div/span/input[1]"
+BUY_PRODUCTS_BUTTON = 'xpath://*[@id="buttonContainer"]'
+CART = 'xpath://*[@id="cartWidget"]'
+ADDED_PRODUCT_SUM = 'xpath://*[@id="spanText"]'
+BUY_CONTINUE = 'xpath://*[@id="continue"]'
+MODAL = 'xpath://*[@id="modal"]/div/div/div/div[2]/div/div[2]'
 # judu.robot
-
+BUS_SCHEDULE = "xpath:/html/body/div[1]/div[2]/div[2]/section/div/div[1]/div/div[1]/div/ul/li[2]"
+SCHEDULE_IFRAME = "xpath:/html/body/div[1]/div[2]/div[2]/div[2]/div/div/iframe"
+EXPRESS_BUSES = "xpath:/html/body/div[3]/div[4]/div[1]/div[3]/div[1]"
+FIRST_EXPRESS_BUS = "xpath:/html/body/div[3]/div[4]/div[1]/div[3]/table[1]/tbody/tr[1]/td/div/a[1]/span[3]"
+FIRST_BUS_STOP = "xpath: /html/body/div[3]/div[4]/div[3]/div[1]/div[3]/div[2]"
 # judu.robot and pigu.robot
 Browser = "Chrome"
 MAX_WINDOW = "Maximize Browser Window"
